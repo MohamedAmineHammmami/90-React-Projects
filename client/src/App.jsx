305e3f8017bf6ba1132f60af51fa4006b55372df
Mohamed Amine Hammami <mohamedaminehammami1994@gmail.com>
2025-02-19 02:13:07 +0100

    calculator functionalities

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState(0);
+  const [result, setResult] = useState("result");
+  console.log("result", result);
+  console.log("input", input);
+
+  const handleOnClick = (el) => {
+    if (el === "clear") {
+      setInput(0);
+      setResult("result");
+    } else if (el === "=") {
+      try {
+        const r = eval(input);
+        setResult(r);
+      } catch (err) {
+        setResult("math error!");
+        setInput(0);
+      }
+    } else {
+      setInput((c) => (c !== 0 ? c + el : el));
+    }
+  };
   return (
     <div className="appBg">
       <div className="container">
         <h1 className="appTitle">Calculator</h1>
         <div className="inputShowScreen">
-          <input type="text" readOnly value={3} />
-          <input type="text" readOnly value={"result"} />
+          <input type="text" readOnly value={input} />
+          <input type="text" readOnly value={result} />
         </div>
         <div className="btnContainer">
           <div>
-            {[7, 8, 9, "/"].map((el, i) => {
-              return <button>{el}</button>;
+            {["7", "8", "9", "/"].map((el, i) => {
+              return (
+                <button key={i} onClick={() => handleOnClick(el)}>
+                  {el}
+                </button>
+              );
             })}
           </div>
           <div>
-            {[4, 5, 6, "*"].map((el, i) => {
-              return <button>{el}</button>;
+            {["4", " 5", "6", "*"].map((el, i) => {
+              return (
+                <button key={i} onClick={() => handleOnClick(el)}>
+                  {el}
+                </button>
+              );
             })}
           </div>
           <div>
-            {[1, 2, 3, "-"].map((el, i) => {
-              return <button>{el}</button>;
+            {["1", "2", "3", "-"].map((el, i) => {
+              return (
+                <button key={i} onClick={() => handleOnClick(el)}>
+                  {el}
+                </button>
+              );
             })}
           </div>
           <div>
-            {[0, "+", "=", "clear"].map((el, i) => {
-              return <button>{el}</button>;
+            {["0", "+", "=", "clear"].map((el, i) => {
+              return (
+                <button key={i} onClick={() => handleOnClick(el)}>
+                  {el}
+                </button>
+              );
             })}
           </div>
         </div>
