bc0d38efdd148a0c59d1467a67c7825c3da6255e
MohamedAmineHammmami <mohamedaminehammami1994@gmail.com>
2025-04-09 10:23:12 +0100

    ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="jokeContainer">
+      <h1>Joke Genarator Using React And Joke API</h1>
+      <button>Click To Generate A New Joke.</button>
+      <p>once upon a time me and you hahahahahaha..</p>
+    </div>
+  );
 }
 
 export default App;
