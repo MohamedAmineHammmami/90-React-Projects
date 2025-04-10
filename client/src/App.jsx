51bb0bdfdf52224554e5901625a06f1514c63108
MohamedAmineHammmami <mohamedaminehammami1994@gmail.com>
2025-04-10 17:52:27 +0100

    passValidor ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import { Eye, EyeOff, CircleOff, CircleCheckBig, Square } from "lucide-react";
 
+const conditions = [
+  " Length more than 8.",
+  "  Contains uppe case letter.",
+  " Contains lower case letter.",
+  " Contains one numerical",
+  " Contains one special character",
+];
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h1>Password Validation</h1>
+      <div className="inputContainer">
+        <input placeholder="Enter your password" />
+        <Eye color="white" size={24} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      {conditions.map((el, i) => {
+        return (
+          <div className="subSection">
+            <CircleOff size={23} color="#000428" key={i} />
+            <span key={i}>{el}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
