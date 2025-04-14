46a35a897c4597709afae9d2bc2ed5d5c726e205
MohamedAmineHammmami <mohamedaminehammami1994@gmail.com>
2025-04-14 15:49:52 +0100

    Location finder Ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import Map from "./map/Map";
 
 function App() {
-  return <div>App</div>;
+  const [currentIP, setCurrentIP] = useState(0);
+  const [error, setError] = useState("");
+  const position = [51.505, -0.09];
+
+  const getTheCurrentIP = () => {
+    fetch("https://api.ipify.org?format=json")
+      .then((res) => res.json())
+      .then((data) => setCurrentIP(data.ip))
+      .catch((err) => setError(err.message));
+  };
+
+  useEffect(() => {
+    getTheCurrentIP();
+  }, []);
+  return (
+    <div className="container">
+      <div className="header">
+        {/* icon */}
+        <h1>LOCATION FINDER</h1>
+      </div>
+      <div className="ipContainer">
+        <span>Find Location By IP Address:</span>
+        <div className="inputContainer">
+          <input type="text" placeholder="Enter your IP address.." />
+          <button>Search</button>
+        </div>
+      </div>
+      <Map />
+    </div>
+  );
 }
 
 export default App;
