9d09712e8a980208902a369324ad03fdbc3a20b4
MohamedAmineHammmami <mohamedaminehammami1994@gmail.com>
2025-04-16 17:05:58 +0100

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Dice from "./components/Dice/Dice";
+import shakeSound from "./assets/shakeSound.mp3";
 
 import "./App.css";
 
 function App() {
   const [diceOne, setDiceOne] = useState(1);
   const [diceTwo, setDiceTwo] = useState(1);
+  const [shake, setShake] = useState(false);
+  const audioRef = useRef();
+
+  const genRandomNum = () => {
+    const unSignArray = new Uint32Array(1);
+    const randomNum = crypto.getRandomValues(unSignArray)[0];
+    return (randomNum % 6) + 1;
+  };
+  const handleDiceRolling = () => {
+    setShake(true);
+    audioRef.current.play();
+    setDiceOne(genRandomNum());
+    setDiceTwo(genRandomNum());
+    setTimeout(() => {
+      setShake(false);
+    }, 500);
+  };
+
   return (
     <div className="diceContainer">
-      <div className="dices">
-        {" "}
-        {[...Array(2)].map((_, i) => {
-          return <Dice key={i} />;
-        })}
+      <audio ref={audioRef} src={shakeSound} controls hidden />
+      <h1 className="header">
+        Your Score is: <span>{diceOne + diceTwo} Points</span>
+      </h1>
+      <div className={shake ? "shake" : "dices"}>
+        <Dice diceRandomNum={diceOne} />
+        <Dice diceRandomNum={diceTwo} />
       </div>
 
-      <button>Roll Dice!</button>
+      <button onClick={handleDiceRolling}>Roll Dice!</button>
     </div>
   );
 }
--- client/src/components/Dice/Dice.jsx
+++ client/src/components/Dice/Dice.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 import "./dice.css";
 import { FaCircle } from "react-icons/fa";
-function Dice() {
+
+const dicePatterns = {
+  1: ["dicePointCenter"],
+  2: ["dicePointRight", "dicePointLeftBottom"],
+  3: ["dicePointCenter", "dicePointRight", "dicePointLeftBottom"],
+
+  4: [
+    "dicePointLeft",
+    "dicePointLeftBottom",
+    "dicePointRight",
+    "dicePointRightBottom",
+  ],
+
+  5: [
+    "dicePointCenter",
+    "dicePointLeft",
+    "dicePointLeftBottom",
+    "dicePointRight",
+    "dicePointRightBottom",
+  ],
+  6: [
+    "dicePointLeft",
+    "dicePointLeftBottom",
+    "dicePointRight",
+    "dicePointRightBottom",
+    "dicePointMiddleLeft",
+    "dicePointMiddleRight",
+  ],
+};
+
+function Dice({ diceRandomNum }) {
   return (
     <div className="dicePointsContainer">
-      <FaCircle size={35} className="dicePointLeft" />
+      {dicePatterns[diceRandomNum].map((el, i) => {
+        return <FaCircle key={i} size={35} className={el} />;
+      })}
     </div>
   );
 }
