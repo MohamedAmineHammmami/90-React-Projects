342882fa7e7e06eca22de2915b3922aab2a79ad8
MohamedAmineHammmami <mohamedaminehammami1994@gmail.com>
2025-04-26 18:20:51 +0100

    flip coin ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import head from "./assets/head.jpg";
+import tail from "./assets/tail.jpg";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="flipCoinContainer">
+      <h1>Flip Coin</h1>
+
+      <img src={head} alt="head" className="img" />
+      {/*  <img src={tail} alt="tail" className="img" /> */}
+      <button>Flip</button>
+      <h2>Out of n flip There have been x Heads y tails</h2>
+    </div>
+  );
 }
 
 export default App;
