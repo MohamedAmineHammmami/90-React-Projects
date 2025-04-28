3981ecc80125989670448529ed950585aa362c52
MohamedAmineHammmami <mohamedaminehammami1994@gmail.com>
2025-04-28 20:30:04 +0100

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
 import "./App.css";
 import BoxContainer from "./components/boxContainer/BoxContainer";
-import ColorValues from "./components/colorValues/ColorValues";
-const rgb = ["RGB: ( RED , GREEN , BLUE )", "RED: 00", "GREEN: 00", "BLUE: 00"];
-const hex = ["HEX: #RRGGBB ", "Red: 00", "GREEN: 00", "BLUE: 00"];
+
+export const MyContext = createContext();
 function App() {
   return (
     <div className="appContainer">
-      <ColorValues colorModel="hex" placeholders={hex} />
       <BoxContainer boxNum={100} />
-      <ColorValues colorModel="rgb" placeholders={rgb} />
     </div>
   );
 }
--- client/src/components/box/Box.jsx
+++ client/src/components/box/Box.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./box.css";
 
+const ColorValues = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+];
 function Box() {
-  return <div className="box"></div>;
+  const [color, setColor] = useState("");
+
+  const genRandomIndex = () => {
+    const unSignedArr = new Uint32Array(1);
+    const randomIndex = crypto.getRandomValues(unSignedArr)[0] % 16;
+    return randomIndex;
+  };
+
+  const genRandomColor = () => {
+    let randomHexColor = "#";
+    for (let i = 0; i < 6; i++) {
+      randomHexColor += ColorValues[genRandomIndex()];
+    }
+    setColor(randomHexColor);
+  };
+
+  useEffect(() => {
+    genRandomColor();
+  }, []);
+
+  return (
+    <div style={{ backgroundColor: color }} onClick={genRandomColor}></div>
+  );
 }
 
 export default Box;
